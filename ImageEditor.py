ab83c6a7e74c8dc897f83f611fd65cee43c81e66
72abdulrehman <abdulrehmanikram9@gmail.com>
2024-05-12 19:10:29 +0500

    Upload FIle Option Added

--- ImageEditor.py
+++ ImageEditor.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Image Editor")
+
 with st.expander("Start Camera"):
     camera_image = st.camera_input("Camera")
 
@@ -10,4 +12,10 @@ if camera_image:
     gray_img = img.convert("L")
 
     st.image(gray_img)
+    
 
+uploaded_image = st.file_uploader("uploaded_image")
+if uploaded_image:
+    img1 = Image.open(uploaded_image)
+    gray_img1 = img1.convert("L")
+    st.image(gray_img1)
